7bb6c50581df0b53f137978739471e6cd2c273d0
maulana <104130367+Maulana389@users.noreply.github.com>
2024-08-07 22:01:33 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,7 @@ app.use(cookieParser());
 const corsOptions = {
     origin: 'http://localhost:5173',
     credentials: true, // Add other origins if needed
+    methods: ["GET","POST","PUT","DELETE"],
 };
 
 app.use(cors(corsOptions));
